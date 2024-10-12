15f1675601a05ebae4f9626b216a1454c9bd24d4
Alin Silcock <at.silcock@gmail.com>
2024-10-11 20:32:52 -0600

    updated models with new rules/associations

--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 from config import db
 
@@ -11,7 +13,9 @@ class GroceryStore(db.Model, SerializerMixin):
     location = db.Column(db.String)
     departments = db.relationship('Department', back_populates="grocery_store")
 
-    
+    employees = association_proxy('departments', 'employees')
+
+    serialize_rules =('-departments.grocery_store', )
 
 class Employee(db.Model, SerializerMixin):
     __tablename__ = 'employees'
@@ -23,6 +27,8 @@ class Employee(db.Model, SerializerMixin):
     work_hours = db.Column(db.String)
     grocery_store_id = db.Column(db.Integer, db.ForeignKey('grocery_store.id'))
     departments = db.relationship('Department', secondary='employee_department', back_populates='employees')
+
+    serialize_rules = ('-departments.employees', '-password_hash') 
 
     @property
     def password(self):
@@ -45,6 +51,8 @@ class Department(db.Model, SerializerMixin):
     grocery_store_id = db.Column(db.Integer, db.ForeignKey('grocery_store.id'))
     grocery_store = db.relationship('GroceryStore', back_populates='departments')
     employees = db.relationship('Employee', secondary='employee_department', back_populates='departments')
+
+    serialize_rules = ('-employees.departments', '-grocery_store.departments')
 
 employee_department = db.Table('employee_department',
     db.Column('employee_id', db.Integer, db.ForeignKey('employees.id')),
