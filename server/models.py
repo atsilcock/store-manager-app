aa20f07a64e52d19e60690f78e7cd13dbdd9f379
Alin Silcock <at.silcock@gmail.com>
2024-10-11 20:28:00 -0600

    updating pipfile

--- server/models.py
+++ server/models.py
@@ -3,4 +3,50 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class GroceryStore(db.Model, SerializerMixin):
+    __tablename__ = 'grocery_store'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    location = db.Column(db.String)
+    departments = db.relationship('Department', back_populates="grocery_store")
+
+    
+
+class Employee(db.Model, SerializerMixin):
+    __tablename__ = 'employees'
+
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
+    user_name = db.Column(db.String, nullable = False)
+    work_hours = db.Column(db.String)
+    grocery_store_id = db.Column(db.Integer, db.ForeignKey('grocery_store.id'))
+    departments = db.relationship('Department', secondary='employee_department', back_populates='employees')
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute.')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # Method to verify password
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+        
+
+class Department(db.Model, SerializerMixin):
+    __tablename__ = 'departments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    grocery_store_id = db.Column(db.Integer, db.ForeignKey('grocery_store.id'))
+    grocery_store = db.relationship('GroceryStore', back_populates='departments')
+    employees = db.relationship('Employee', secondary='employee_department', back_populates='departments')
+
+employee_department = db.Table('employee_department',
+    db.Column('employee_id', db.Integer, db.ForeignKey('employees.id')),
+    db.Column('department_id', db.Integer, db.ForeignKey('departments.id'))
+) 
