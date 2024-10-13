a24d479f84583c3d2c49c427b5b3a2d9df9ecd5c
Alin Silcock <at.silcock@gmail.com>
2024-10-13 14:51:21 -0600

    updating About.js

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,22 +1,48 @@
 import React, { useState } from 'react';
-import { Formik, Form, Field } from 'formik';
+import { useForMik, Formik, Field } from 'formik';
 import EmployeeCard from './EmployeeCard';
 
 function About({ employees }) {
-  const [selectedEmployeeId, setSelectedEmployeeId] = useState(null);
+  const [selectedEmployee, setSelectedEmployee] = useState(null);
 
+  const handleEmployeeSelection = (employeeId) => {
+    const selected = employees.find(employee => employee.id === parseInt(employeeId));
+    setSelectedEmployee(selected)
+  };
 
-
-  const selectedEmployee = employees.find(employee => employee.id === parseInt(selectedEmployeeId));
+  const formik = useForMik ({
+    initialValues: {
+        employeeId: ''
+    },
+    onSubmit: (values) => {
+        handleEmployeeSelection(values.employeeId)
+    }
+  })
 
   return (
     <div>
       <h1>Manage Employees</h1>
       <h3>Select Employees or Departments</h3>
 
-      <Formik
-        initialVulues = {selectedEmployee}
-
+        <form onSubmit={formik.handleSubmit}>
+        <label>Employees: </label>
+        <select
+          name="employeeId"
+          value={formik.values.employeeId}
+          onChange={(e) => {
+            formik.handleChange(e); // Let Formik handle form state
+            handleEmployeeSelection(e.target.value); // Handle employee selection logic
+          }}
+        >
+          <option value="" disabled>Select an employee</option>
+          {employees.map(employee => (
+            <option key={employee.id} value={employee.id}>
+              {employee.name} | {employee.role}
+            </option>
+          ))}
+        </select>
+      </form>
+    
       {selectedEmployee && <EmployeeCard employee={selectedEmployee} />}
     </div>
   );
