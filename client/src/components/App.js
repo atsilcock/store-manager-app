9d0d2f0bef269203813f90f6a07099231bfa429e
Alin Silcock <at.silcock@gmail.com>
2024-10-13 20:10:58 -0600

    updating home page in client/src - displays grocery stores

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -16,6 +16,7 @@ function App() {
       .then(data => setEmployees(data))
   }, [])
 
+
   return (
     <Router>
       <NavBar />
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,27 @@
 import React, { useEffect, useState } from "react";
 
-function Home({ employees, stores }) {
+function Home() {
+    const [stores, setStores] = useState([])
+
+    
+
  
+    useEffect(() => {
+        fetch("http://127.0.0.1:5555/store",)
+        .then(response => response.json())
+        .then(data => setStores(data))
+    },[])
+
+    const storeList = stores.map(store => {
+        return <ul key={store.id}> {store.name} Grocery | Store ID: {store.id} | {store.location} </ul>
+
+    })
       
     return (
-      <div></div>
+      <div style={{ marginTop: '20px', padding: '10px', border: '1px solid #ccc' }}>
+        <h1>Grocery Stores</h1>
+        {storeList}
+      </div>
     )
 }
 
